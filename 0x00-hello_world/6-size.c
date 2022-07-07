6797eb025390f91876e8754cec9001b611e91d5c
cyphproxy <cypherpeters@icloud.com>
2022-07-07 00:07:06 -0700

    edited main

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry point
- * return: (0)
+ * main - prints the size of various types
+ * return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
