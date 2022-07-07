cb6769ef240ed5cf0279729aad13c955f1ad4208
cyphproxy <cypherpeters@icloud.com>
2022-07-07 00:32:35 -0700

    compile error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,14 @@
 int main(void)
 {
 int i;
-long int I;
-long long int II;
+long int l;
+long long int ll;
 float d;
 char c;
 printf("size of a char: %lu byte(s)\n", sizeof(c));
 printf("size of an int: %lu byte(s)\n", sizeof(i));
-printf("size of a long int: %lu byte(s)\n", sizeof(I));
-printf("size of a long long int: %lu byte(s)\n", sizeof(II));
+printf("size of a long int: %lu byte(s)\n", sizeof(l));
+printf("size of a long long int: %lu byte(s)\n", sizeof(ll));
 printf("size of a float: %lu byte(s)\n", sizeof(d));
-retrun(0)
+retrun(0);
 }
